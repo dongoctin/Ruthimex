430d0da260d7aaa0747934dbf0db849bb00eea2a
dongoctin <tinngocdo1995@gmail.com>
2023-06-01 22:33:33 +0700

    upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -72,27 +72,27 @@ export default function Header() {
                         <i class="fa-solid fa-bars"></i>
                     </Dropdown.Toggle>
                     <Dropdown.Menu className="sub-menu">
-                        <NavLink to="/homepage" onClick={hideMenu}><i class="fa-solid fa-house"></i>HOME PAGE</NavLink>
-                        <NavLink to="/homepage" onClick={handleAboutUsClick}><i class=" fa-regular fa-id-card"></i> <span>ABOUT US</span>
+                        <NavLink to="/homepage" onClick={hideMenu}><i class="fa-solid fa-house"></i>TRANG CHỦ</NavLink>
+                        <NavLink to="/homepage" onClick={handleAboutUsClick}><i class=" fa-regular fa-id-card"></i> <span>VỀ CHÚNG TÔI</span>
                             <ul className='sub-menu-aboutus'>
                                 <li>
-                                    <NavLink show={showMenu} onClick={hideMenu} to="/aboutus/corevalue"><i class="fa-solid fa-snowflake"></i> <span>CORE VALUE</span></NavLink>
+                                    <NavLink show={showMenu} onClick={hideMenu} to="/aboutus/corevalue"><i class="fa-solid fa-snowflake"></i> <span>GIÁ TRỊ CỐT LÕI</span></NavLink>
                                 </li>
                                 <li>
-                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/managerteam"><i class="fa-solid fa-snowflake"></i> <span>MANAGEMENT TEAM</span></NavLink>
+                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/managerteam"><i class="fa-solid fa-snowflake"></i> <span>ĐỘI NGŨ QUẢN LÝ</span></NavLink>
                                 </li>
                                 <li>
-                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/history"><i class="fa-solid fa-snowflake"></i><span>HISTORY</span></NavLink>
+                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/history"><i class="fa-solid fa-snowflake"></i><span>LỊCH SỬ HÌNH THÀNH</span></NavLink>
                                 </li>
                                 <li>
-                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/branchsystems"><i class="fa-solid fa-snowflake"></i><span>BRANCH SYSTEMS</span></NavLink>
+                                    <NavLink show={showMenu} onClick={hideMenu} to="aboutus/branchsystems"><i class="fa-solid fa-snowflake"></i><span>CÁC XÍ NGHIỆP TRỰC THUỘC</span></NavLink>
                                 </li>
                             </ul>
                         </NavLink>
-                        <NavLink to="/products" onClick={hideMenu}><i class="fa-brands fa-product-hunt"></i>PRODUCTS</NavLink>
-                        <NavLink to="/capacityprofile" onClick={hideMenu}><i class="fa-solid fa-paper-plane"></i>CAPACITY PROFILE</NavLink>
-                        <NavLink to="/news" onClick={hideMenu}><i class="fa-regular fa-newspaper"></i>NEWS</NavLink>
-                        <NavLink to="/contactus" onClick={hideMenu}><i class="fa-solid fa-envelope"></i>CONTACT US</NavLink>
+                        <NavLink to="/products" onClick={hideMenu}><i class="fa-brands fa-product-hunt"></i>SẢN PHẨM</NavLink>
+                        <NavLink to="/capacityprofile" onClick={hideMenu}><i class="fa-solid fa-paper-plane"></i>HỒ SƠ NĂNG LỰC</NavLink>
+                        <NavLink to="/news" onClick={hideMenu}><i class="fa-regular fa-newspaper"></i>TIN TỨC</NavLink>
+                        <NavLink to="/contactus" onClick={hideMenu}><i class="fa-solid fa-envelope"></i>LIÊN HỆ</NavLink>
                     </Dropdown.Menu>
                 </Dropdown>
 
@@ -150,42 +150,43 @@ export default function Header() {
 
             {/* .............................. */}
             <nav className="nav-links">
-                <NavLink className="btn btn-info" to="/homepage">HOME PAGE</NavLink>
+                <NavLink className="btn btn-info" to="/homepage">TRANG CHỦ</NavLink>
                 <Dropdown show={showDropdown} onMouseEnter={handleDropdownClick} onMouseLeave={handleDropdownClick}>
-                    <NavLink className="btn btn-info" to="/aboutus" >ABOUT US</NavLink>
+                    <NavLink className="btn btn-info" to="/aboutus" >VỀ CHÚNG TÔI</NavLink>
                     <Dropdown.Menu >
-                        <NavLink to="/aboutus/corevalue" className="dropdown-item about-us-1"> <i className="fa fa-chevron-right"></i> Core Value </NavLink>
-                        <NavLink to="aboutus/managerteam" className="dropdown-item"><i className="fa fa-chevron-right"></i> Management Team</NavLink>
-                        <NavLink to="aboutus/history" className="dropdown-item"> <i className="fa fa-chevron-right"></i> History</NavLink>
-                        <NavLink to="aboutus/branchsystems" className="dropdown-item" > <i className="fa fa-chevron-right"></i> Branch Systems</NavLink>
+                        <NavLink to="/aboutus/corevalue" className="dropdown-item about-us-1"> <i className="fa fa-chevron-right"></i>Giá trị cốt lõi </NavLink>
+                        <NavLink to="aboutus/managerteam" className="dropdown-item"><i className="fa fa-chevron-right"></i>Đội ngũ quản lý</NavLink>
+                        <NavLink to="aboutus/history" className="dropdown-item"> <i className="fa fa-chevron-right"></i> Lịch sử</NavLink>
+                        <NavLink to="aboutus/branchsystems" className="dropdown-item" > <i className="fa fa-chevron-right"></i>Các xí nghiệp trực tiếp</NavLink>
                     </Dropdown.Menu >
                 </Dropdown>
                 <Dropdown show={showDropdown1} onMouseEnter={handleDropdownClick1} onMouseLeave={handleDropdownClick1}>
-                    <NavLink className="btn btn-info" to="/products">PRODUCTS</NavLink>
+                    <NavLink className="btn btn-info" to="/products">SẢN PHẨM</NavLink>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1"> <i className="fa fa-chevron-right"></i> Construction and Minerals </Dropdown.Item>
-                        <Dropdown.Item href="#/action-2"><i className="fa fa-chevron-right"></i> Auto and Moto</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i> Power electronics</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i> Water supply</Dropdown.Item>
-                        <Dropdown.Item href="#/action-5"><i className="fa fa-chevron-right"></i>  Mechanical</Dropdown.Item>
-                        <Dropdown.Item href="#/action-6"><i className="fa fa-chevron-right"></i>  Consumer products</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1"> <i className="fa fa-chevron-right"></i>Xây dựng và khai khoán </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2"><i className="fa fa-chevron-right"></i>Ô tô - xe máy</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i>Điện - điện tử</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i>Cấp thoát nước</Dropdown.Item>
+                        <Dropdown.Item href="#/action-5"><i className="fa fa-chevron-right"></i>Máy móc - thiết bị</Dropdown.Item>
+                        <Dropdown.Item href="#/action-6"><i className="fa fa-chevron-right"></i>Sản phẩm tiêu dùng</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <Dropdown show={showDropdown2} onMouseEnter={handleDropdownClick2} onMouseLeave={handleDropdownClick2}>
-                    <NavLink className="btn btn-info" id='capacityprofile' to="/capacityprofile">CAPACITY PROFILE</NavLink>
+                    <NavLink className="btn btn-info" id='capacityprofile' to="/capacityprofile">HỒ SƠ NĂNG LỰC</NavLink>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1"><i className="fa fa-chevron-right"></i>  Certificate </Dropdown.Item>
-                        <Dropdown.Item href="#/action-2"><i className="fa fa-chevron-right"></i>  Awards</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i>  Facilities</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i>  Production technology</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1"><i className="fa fa-chevron-right"></i>Chứng nhận</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2"><i className="fa fa-chevron-right"></i>Giải thưởng</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i>Cơ sở vật chất</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i>Công nghệ sản xuất</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <Dropdown show={showDropdown3} onMouseEnter={handleDropdownClick3} onMouseLeave={handleDropdownClick3}>
-                    <NavLink className="btn btn-info " id="news" to="/news">NEWS</NavLink>
+                    <NavLink className="btn btn-info " id="news" to="/news">TIN TỨC</NavLink>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1"><i className="fa fa-chevron-right"></i> Local news </Dropdown.Item>
-                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i>  UNI0N activities</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i>   Market news</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1"><i className="fa fa-chevron-right"></i>Tin tức nội bộ</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2"><i className="fa fa-chevron-right"></i>Tuyển dụng</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3"><i className="fa fa-chevron-right"></i>Hoạt động đoàn thể</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4"><i className="fa fa-chevron-right"></i>Tin tức thị trường</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <NavLink className="btn btn-info" to="/contactus">CONTACT US</NavLink>
